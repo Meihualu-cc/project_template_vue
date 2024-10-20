b530c3f54204bdbce4acca43850a9941aef4aeee
Meihualu-cc <78076901+Meihualu-cc@users.noreply.github.com>
2024-10-20 16:55:00 +0800

    Add files via upload

---
+++ src/http/request.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import type {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+const service: AxiosInstance = axios.create({
+  baseURL: Boolean(import.meta.env.VITE_APP_USE_MOCK)
+  ? import.meta.env.VITE_APP_MOCK_BASEURL
+  : import.meta.env.VITE_APP_API_BASEURL,
+    timeout: 15000,
+  });
+
+// axios实例拦截请求
+service.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+// axios实例拦截响应
+service.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
+
+export default service
